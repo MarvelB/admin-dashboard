d6cf2ff388e1c1b34e9bfd4c5547a4a7e915f049
Bryan <bryan@devlift.io>
2023-03-16 22:08:20 -0400

    added more commands and some templates

---
+++ src/template/component/TemplateName.tsx
@@ -0,0 +1,13 @@
+import './TemplateName.css';
+
+interface TemplateNameProps {}
+
+const TemplateName = ({ }: TemplateNameProps) => {
+  return (
+    <div>
+      <h2>TemplateName</h2>
+    </div>
+  );
+}
+
+export default TemplateName;
