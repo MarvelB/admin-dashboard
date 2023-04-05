b194e7c4a247f27ca8b2d92813c1629ae60d40ec
Bryan <bryan@devlift.io>
2023-04-04 21:43:09 -0400

    added bar component

---
+++ src/components/BarComponent/BarComponent.tsx
@@ -0,0 +1,11 @@
+interface BarComponentProps {}
+
+const BarComponent = ({}: BarComponentProps) => {
+    return (
+        <div>
+            <h2>BarComponent</h2>
+        </div>
+    );
+};
+
+export default BarComponent;
