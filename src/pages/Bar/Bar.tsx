c3c99acf6f52652d5d54807fd0ed171c9623cd09
Bryan <bryan@devlift.io>
2023-04-04 22:24:00 -0400

    finished working on the bar page

--- src/pages/Bar/Bar.tsx
+++ src/pages/Bar/Bar.tsx
@@ -1,10 +1,18 @@
+import { Box } from "@mui/material";
+import BarComponent from "components/BarComponent/BarComponent";
+import Header from "components/Header/Header";
+
 interface BarProps {}
 
 const Bar = ({}: BarProps) => {
     return (
-        <div>
-            <h2>Bar</h2>
-        </div>
+        <Box m="20px">
+            <Header title="BAR CHART" subTitle="Static data bar chart" />
+
+            <Box height="75vh">
+                <BarComponent />
+            </Box>
+        </Box>
     );
 };
 
