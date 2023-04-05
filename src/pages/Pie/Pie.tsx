965b7333ba0a78851929a30104c49554ba163ea8
Bryan <bryan@devlift.io>
2023-04-04 23:31:54 -0400

    finished working on piechart page

--- src/pages/Pie/Pie.tsx
+++ src/pages/Pie/Pie.tsx
@@ -1,10 +1,18 @@
+import { Box } from "@mui/material";
+import Header from "components/Header/Header";
+import PieChartComponent from "components/PieChartComponent/PieChartComponent";
+
 interface PieProps {}
 
 const Pie = ({}: PieProps) => {
     return (
-        <div>
-            <h2>Pie</h2>
-        </div>
+        <Box m="20px">
+            <Header title="PIE CHART" subTitle="Static data pie chart" />
+
+            <Box height="75vh">
+                <PieChartComponent />
+            </Box>
+        </Box>
     );
 };
 
