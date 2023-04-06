750a3ab4380bc5227f9654ac01b3838978ffd1bc
Bryan <bryan@devlift.io>
2023-04-06 17:48:14 -0400

    finished working on the geography page

--- src/pages/Geography/Geography.tsx
+++ src/pages/Geography/Geography.tsx
@@ -1,10 +1,29 @@
+import { Box, useTheme } from "@mui/material";
+import GeographyChartComponent from "components/GeographyChartComponent/GeographyChartComponent";
+import Header from "components/Header/Header";
+import { tokens } from "theme";
+
 interface GeographyProps {}
 
 const Geography = ({}: GeographyProps) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
     return (
-        <div>
-            <h2>Geography</h2>
-        </div>
+        <Box m="20px">
+            <Header
+                title="GEOGRAPHY CHART"
+                subTitle="Static data geography chart"
+            />
+
+            <Box
+                height="75vh"
+                border={`1px solid ${colors.grey[100]}`}
+                borderRadius="4px"
+            >
+                <GeographyChartComponent />
+            </Box>
+        </Box>
     );
 };
 
